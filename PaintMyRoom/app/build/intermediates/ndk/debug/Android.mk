4f507fc5183bc33d0d3ad4daa0fe4744e499141e
Chocobean <berck.sam@gmail.com>
2015-12-14 22:55:25 +0100

    first commit

---
+++ PaintMyRoom/app/build/intermediates/ndk/debug/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mz11-native
+LOCAL_LDFLAGS := -Wl,--build-id
+LOCAL_SRC_FILES := \
+	/home/choco/AndroidStudioProjects/sobel2/app/src/main/jni/Android.mk \
+	/home/choco/AndroidStudioProjects/sobel2/app/src/main/jni/native.c \
+
+LOCAL_C_INCLUDES += /home/choco/AndroidStudioProjects/sobel2/app/src/main/jni
+LOCAL_C_INCLUDES += /home/choco/AndroidStudioProjects/sobel2/app/src/debug/jni
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ PaintMyRoom/app/build/intermediates/ndk/debug/obj/local/mips/objs/mz11-native/home/choco/AndroidStudioProjects/sobel2/app/src/main/jni/native.o.d
@@ -0,0 +1,26 @@
+/home/choco/AndroidStudioProjects/sobel2/app/build/intermediates/ndk/debug/obj/local/mips/objs/mz11-native//home/choco/AndroidStudioProjects/sobel2/app/src/main/jni/native.o: \
+ /home/choco/AndroidStudioProjects/sobel2/app/src/main/jni/native.c \
+ /home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/jni.h \
+ /home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/sys/cdefs.h \
+ /home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/sys/cdefs_elf.h \
+ /home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/android/api-level.h \
+ /home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/string.h \
+ /home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/malloc.h \
+ /home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/xlocale.h \
+ /home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/android/log.h
+
+/home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/jni.h:
+
+/home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/sys/cdefs.h:
+
+/home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/sys/cdefs_elf.h:
+
+/home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/android/api-level.h:
+
+/home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/string.h:
+
+/home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/malloc.h:
+
+/home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/xlocale.h:
+
+/home/choco/Android/Sdk/ndk-bundle/platforms/android-21/arch-mips/usr/include/android/log.h:
