896456a3a75f195cc9a327c6ddc94c5b2a2b47f3
Chocobean <berck.sam@gmail.com>
2015-12-14 23:23:50 +0100

    commit

--- PaintMyRoom/app/build/intermediates/ndk/debug/Android.mk
+++ PaintMyRoom/app/build/intermediates/ndk/debug/Android.mk
@@ -4,10 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := mz11-native
 LOCAL_LDFLAGS := -Wl,--build-id
 LOCAL_SRC_FILES := \
-	/home/choco/AndroidStudioProjects/sobel2/app/src/main/jni/Android.mk \
-	/home/choco/AndroidStudioProjects/sobel2/app/src/main/jni/native.c \
+	/home/choco/AndroidStudioProjects/PaintMyRoom/app/src/main/jni/Android.mk \
+	/home/choco/AndroidStudioProjects/PaintMyRoom/app/src/main/jni/native.c \
 
-LOCAL_C_INCLUDES += /home/choco/AndroidStudioProjects/sobel2/app/src/main/jni
-LOCAL_C_INCLUDES += /home/choco/AndroidStudioProjects/sobel2/app/src/debug/jni
+LOCAL_C_INCLUDES += /home/choco/AndroidStudioProjects/PaintMyRoom/app/src/main/jni
+LOCAL_C_INCLUDES += /home/choco/AndroidStudioProjects/PaintMyRoom/app/src/debug/jni
 
 include $(BUILD_SHARED_LIBRARY)
